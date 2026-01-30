6d8531ca43869f3ed73d483933b6899145aadff2
Carlos Magno Barreto Gomes <carlosmagno.gomes@LATINAMERICA.local>
2026-01-30 09:43:40 -0300

    Noto item na lista

--- 9-lista.py
+++ 9-lista.py
@@ -1,4 +1,4 @@
 filmMatrix = ["Matrix", 1999, 8.7, True]
 print(filmMatrix)
 
-filmsList = ["Inception", "The Dark Knight", "Interstellar", "Dunkirk", "Star Wars"]
+filmsList = ["Inception", "The Dark Knight", "Interstellar", "Dunkirk", "Star Wars", "The Godfather"]
